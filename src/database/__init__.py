70c2431a18cb04f026006b9ae453d13e16ec72ee
Illia Redka <illaredka8@gmail.com>
2026-07-08 11:59:17 +0300

    Add user profile and token database models

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -3,4 +3,10 @@ from src.database.models.accounts import (
     UserModel,
     UserGroupModel,
     UserGroupEnum,
+    ActivationTokenModel,
+    PasswordResetTokenModel,
+    RefreshTokenModel,
+    UserProfileModel,
 )
+from src.database.validators import accounts as accounts_validators
+from src.database.session import get_async_session as get_db
